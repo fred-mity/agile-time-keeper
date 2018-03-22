5dcdb9d86ad23f21bf1cc87b9ebf6dbe460123c9
zolenas <zol.hen.as@gmail.com>
2018-03-23 00:20:28 +0100

    Test init - First test

---
+++ test/index.spec.js
@@ -0,0 +1,12 @@
+
+var assert = require('assert');
+
+describe('Index', function() {
+
+    describe('Init', function() {
+        it('should initialise our page', function() {
+            var a = 3;
+            assert.equal(a*2, 6, 'Test n\'a pas fonctionné');
+        })
+    })
+})
